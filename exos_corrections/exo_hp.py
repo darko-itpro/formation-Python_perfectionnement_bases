c41fa6f3b9aca70e8a17f9409ecba91792937edd
Darko Stankovski <darko.itpro@gmaiL.com>
2024-11-20 09:26:00 +0100

    ajout exo hp

---
+++ exos_corrections/exo_hp.py
@@ -0,0 +1,8 @@
+potter_movies = [["The Philosopher's Stone", 152, True],
+                 ["The Chamber of Secrets", 161, True],
+                 ["The Prisoner of Azkaban", 142, False],
+                 ["the Goblet of Fire", 157, True],
+                 ["the Order of the Phoenix", 138, False],
+                 ["the Half-Blood Prince", 153, True],
+                 ["the Deathly Hallows – Part 1", 126, False],
+                 ("the Deathly Hallows – Part 2", 130, False)]
