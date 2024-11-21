d3fb698eb00f7738e23147337b0413a387010b18
Darko Stankovski <darko.itpro@gmaiL.com>
2024-11-21 16:59:42 +0100

    Exo closure avec dict

--- exos_corrections/exo_closure.py
+++ exos_corrections/exo_closure.py
@@ -1,16 +1,19 @@
 import time
+from collections import OrderedDict
 
 def cached_computation():
-    cached_value = (0, 0)
+    cached_value = OrderedDict()
 
     def long_call(value:int):
-        nonlocal cached_value
-        if value == cached_value[0]:
-            result = cached_value[1]
-        else:
+        try:
+            result = cached_value[value]
+            cached_value.move_to_end(value)
+        except KeyError:
             time.sleep(1)
             result = value ** 2
-            cached_value = (value, result)
+            cached_value[value] = result
+            if len(cached_value) > 5:
+                print(cached_value.popitem(last=False))
 
         return result
 
@@ -20,3 +23,8 @@ long_call = cached_computation()
 print(long_call(2)) # 4 en 1 seconde
 print(long_call(4)) # 16 en 1 seconde
 print(long_call(4)) # 16 instantané
+print(long_call(2)) # 4 en 1 seconde
+print(long_call(5))
+print(long_call(6))
+print(long_call(7))
+print(long_call(8))
