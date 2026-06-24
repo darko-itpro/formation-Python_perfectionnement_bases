b48fa72c17171dcef7dcfe29b49481ea43c3bb90
Darko Stankovski <darko.itpro@gmail.com>
2026-06-24 11:20:15 +0200

    Trois premiers tests

--- tests/pyflix/test_mediatheque.py
+++ tests/pyflix/test_mediatheque.py
@@ -4,3 +4,13 @@ from pylib.pyflix.mediatheque import TvShow
 def test_show_title_case():
     my_show = TvShow("one piece")
     assert my_show.name == "One Piece"
+
+def test_episodes_it_attribute():
+    my_show = TvShow("one piece")
+    assert my_show.episodes == []
+
+def test_add_one_episode():
+    my_show = TvShow("one piece")
+    my_show.add_episode("Grand Line", 2, 1)
+    assert len(my_show.episodes) == 1
+    assert my_show.episodes[0].title == "Grand Line"
